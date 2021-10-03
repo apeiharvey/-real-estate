cd758b16bf76ba967fa251979ecbaab8783a37ff
apeiharvey <hjoehanes@gmail.com>
2021-10-03 20:40:45 +0700

    add meta viewport

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
+<meta charset="utf-8">
 <html class="no-js" lang="zxx">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="robots" content="noindex, follow" />
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <head>
 	@include('frontend.layouts.head')	
 </head>
