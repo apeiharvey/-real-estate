f0f309af67a8e690bc0501ab67c30e937f0e737c
tommyfq <tommyquiko@gmail.com>
2021-10-14 20:27:47 +0700

    Change logo favicon

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -4,7 +4,7 @@
 <title>@yield('title')</title>
 <!-- Favicon -->
 <!-- Place favicon.png in the root directory -->
-<link rel="shortcut icon" href="{{asset('frontend/img/favicon.png')}}" type="image/x-icon" />
+<link rel="shortcut icon" href="{{asset('frontend/img/HIVE_LOGO.png')}}" type="image/x-icon" />
 <!-- Font Icons css -->
 <link rel="stylesheet" href="{{asset('frontend/css/font-icons.css')}}">
 <!-- plugins css -->
