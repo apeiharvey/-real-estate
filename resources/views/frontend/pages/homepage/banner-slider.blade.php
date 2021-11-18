a6f296d41c1f15e90bd8ae6b76693c1e3ce08ba2
apeiharvey <hjoehanes@gmail.com>
2021-11-18 23:21:20 +0700

    banner slider

--- resources/views/frontend/pages/homepage/banner-slider.blade.php
+++ resources/views/frontend/pages/homepage/banner-slider.blade.php
@@ -1,6 +1,7 @@
 <div class="ltn__slider-area ltn__slider-3">
     <div class="ltn__slide-one-active banner slick-slide-arrow-1 slick-slide-dots-1">
         @if(isset($banner) && count($banner) > 0)
+            @php $counter=0; @endphp
             @foreach($banner as $val)
                 <div class="slide-item-img">
                     @if(isset($val->url))
@@ -11,13 +12,14 @@
                         {{-- <img style="width:100%" src="{{ENV('APP_ASSET_URL').$val->photo}}" title="{{$val->description}}"/> --}}
                         @elseif($val->type == 'video')
                         <div class="item youtube-sound">
-                            <iframe class="embed-player" style="width:100%; height:450px" src="{{$val->url.'?autoplay=1&enablejsapi=1&mute=1'}}"></iframe>
+                            <div class="embed-player" id="vid-{{$counter}}" data-vid="{{$val->url}}" style="width:100%; height:450px"></div>
                         </div>
                         @endif
                     @else
                         <img style="width:100%" src="{{asset($val->photo)}}"/>
                     @endif
                 </div>
+                @php $counter++; @endphp
             @endforeach
         @endif
     </div>
@@ -39,7 +41,6 @@
                     autoplaySpeed: 5000,
                     slidesToScroll: 1,
                     dots: false,
-                    dotsClass: "home-banner-dots",
                     fade: true,
                     autoplay: true,
                     focusOnSelect: true,
@@ -47,21 +48,18 @@
                     pauseOnHover: true,
                     pauseOnFocus: true,
                     arrows: true,
-                    prevArrow: "#btn-prev-home-banner",
-                    nextArrow: "#btn-next-home-banner",
                 });
 
                 $homeBannerSlide.on(
                     "beforeChange",
                     function (event, slick, currentSlide, nextSlide) {
-                        console.log(event, slick, currentSlide, nextSlide);
-                        if ($(`#yt-${nextSlide}`).length == 1) {
+                        if ($(`#vid-${nextSlide}`).length == 1) {
                             if (ytPlayers[nextSlide] == null) {
-                                let videoId = $(`#yt-${nextSlide}`).attr(
-                                    "data-ytid"
+                                let videoId = $(`#vid-${nextSlide}`).attr(
+                                    "data-vid"
                                 );
 
-                                player = new YT.Player(`yt-${nextSlide}`, {
+                                player = new YT.Player(`vid-${nextSlide}`, {
                                     videoId,
                                     width: "100%",
                                     height: "100%",
@@ -84,7 +82,7 @@
                             }
                         }
 
-                        if ($(`#yt-${currentSlide}`).length == 1) {
+                        if ($(`#vid-${currentSlide}`).length == 1) {
                             let state = ytPlayers[currentSlide].getPlayerState();
 
                             if (state == 1) {
@@ -99,123 +97,4 @@
         section.init();
     }
 </script>
-{{-- <script>
-    var slideWrapper = $(".banner"),
-        iframes = slideWrapper.find('.embed-player'),
-        lazyImages = slideWrapper.find('.slide-image'),
-        lazyCounter = 0;
-
-    function postMessageToPlayer(player, command){
-        if (player == null || command == null) return;
-        player.contentWindow.postMessage(JSON.stringify(command), "*");
-        console.log(player.contentWindow.postMessage(JSON.stringify(command), "*"))
-    }
-    function playPauseVideo(slick, control){
-        var currentSlide, slideType, startTime, player, video;
-
-        currentSlide = slick.find('div.item');
-        slideType = currentSlide.attr("class").split(" ")[1];
-        player = currentSlide.find("iframe").get(0);
-        startTime = currentSlide.data("video-start");
-        if (slideType === "youtube-sound") {
-            switch (control) {
-            case "play":
-                postMessageToPlayer(player, {
-                "event": "command",
-                "func": "playVideo"
-                });
-                break;
-            case "pause":
-                postMessageToPlayer(player, {
-                "event": "command",
-                "func": "pauseVideo"
-                });
-                break;
-            }
-        }  else if (slideType === "youtube") {
-            switch (control) {
-            case "play":
-                postMessageToPlayer(player, {
-                "event": "command",
-                "func": "mute"
-                });
-                postMessageToPlayer(player, {
-                "event": "command",
-                "func": "playVideo"
-                });
-                break;
-            case "pause":
-                postMessageToPlayer(player, {
-                "event": "command",
-                "func": "pauseVideo"
-                });
-                break;
-            }
-        }
-    }
-    // Resize player
-    function resizePlayer(iframes, ratio) {
-        if (!iframes[0]) return;
-        var win = $(".main-slider"),
-            width = win.width(),
-            playerWidth,
-            height = win.height(),
-            playerHeight,
-            ratio = ratio || 16/9;
-
-        iframes.each(function(){
-            var current = $(this);
-            if (width / ratio < height) {
-            playerWidth = Math.ceil(height * ratio);
-            current.width(playerWidth).height(height).css({
-                left: (width - playerWidth) / 2,
-                top: 0
-                });
-            } else {
-            playerHeight = Math.ceil(width / ratio);
-            current.width(width).height(playerHeight).css({
-                left: 0,
-                top: (height - playerHeight) / 2
-            });
-            }
-        });
-    }
-
-    // DOM Ready
-    $(function() {
-    // Initialize
-    slideWrapper.on("init", function(slick){
-        slick = $(slick.currentTarget);
-        setTimeout(function(){
-        playPauseVideo(slick,"play");
-        }, 1000);
-        resizePlayer(iframes, 16/9);
-    });
-    slideWrapper.on("beforeChange", function(event, slick) {
-        slick = $(slick.$slider);
-        playPauseVideo(slick,"pause");
-    });
-    slideWrapper.on("afterChange", function(event, slick) {
-        slick = $(slick.$slider);
-        playPauseVideo(slick,"pause");
-    });
-
-    //start the slider
-    slideWrapper.slick({
-        // fade:true,
-        autoplaySpeed:4000,
-        autoplay: true,
-        lazyLoad:"progressive",
-        speed:600,
-        arrows:false,
-        dots:false,
-        cssEase:"cubic-bezier(0.87, 0.03, 0.41, 0.9)"
-    });
-    });
-
-    // Resize event
-    $(window).on("resize.slickVideoPlayer", function(){  
-    resizePlayer(iframes, 16/9);
-    });
-</script> --}}
 @endpush
